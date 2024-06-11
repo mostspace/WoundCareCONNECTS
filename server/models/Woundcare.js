a243c1479fff441b41e0b93277a3b53e4321ad9a
mostspace <mostspace1027@gmail.com>
2024-06-11 09:02:23 -0700

    login

--- server/models/Woundcare.js
+++ server/models/Woundcare.js
@@ -22,20 +22,7 @@ const WoundcareSchema = new mongoose.Schema({
     timestamps: true,
 });
 
-// Pre-save hook to hash passwords
-WoundcareSchema.pre('save', async function(next) {
-    const user = this;
-    if (user.isModified('password')) {
-        user.password = await bcrypt.hash(user.password, 8);
-    }
-    next();
-});
 
-// Method to compare password
-WoundcareSchema.methods.isValidPassword = async function(password) {
-    const user = this;
-    return bcrypt.compare(password, user.password);
-};
 
 const WoundcareModel = mongoose.model("User", WoundcareSchema);
 module.exports = WoundcareModel;
